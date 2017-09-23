dfc42823818a215f58aacd6ab431d16d9735bba5
chaospredictor <kuzi81@gmail.com>
2017-09-23 17:19:40 +0300

    Inceptionv3 model added

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Master on 23/09/2017.
+//  Copyright © 2017 Master. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
