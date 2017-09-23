333c7718d6fbc85ad2c3036dcdb5ad52a9f54624
chaospredictor <kuzi81@gmail.com>
2017-09-23 17:44:56 +0300

    imagePicker configuration done

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -12,11 +12,22 @@ import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
 
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
