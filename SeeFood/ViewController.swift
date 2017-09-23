8c342f175c06b0464b63a87390f428450abde859
chaospredictor <kuzi81@gmail.com>
2017-09-23 21:55:35 +0300

    print result to the consule

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         imagePicker.delegate = self
         imagePicker.sourceType = .photoLibrary
-        //imagePicker.sourceType = .camera
+        //imagePicker.sourceType = .camera //for real phone
         imagePicker.allowsEditing = false
     }
 
@@ -29,8 +29,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         print("finish")
         if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            
+            detect(image: ciimage)
         }
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Could not ... ")
+            }
+            print(result)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+            fatalError("Could not perform handler")
+        }
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
